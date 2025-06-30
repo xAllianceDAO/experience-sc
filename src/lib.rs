1c3a00fa8413d5ea885cfe954f4b1567cca90c29
Micha Vie <micha@vleap.ai>
2025-06-30 17:57:31 +0200

    add tier system

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 #![no_std]
 
 multiversx_sc::imports!();
+multiversx_sc::derive_imports!();
+
+#[type_abi]
+#[derive(TopEncode, TopDecode, Clone)]
+pub struct Tier<M: ManagedTypeApi> {
+    pub name: ManagedBuffer<M>,
+    pub min_threshold: BigUint<M>,
+    pub voting_power: BigUint<M>,
+}
 
 #[multiversx_sc::contract]
 pub trait ExperienceContract {
@@ -17,39 +26,33 @@ pub trait ExperienceContract {
     fn get_dao_vote_weight_view(
         &self,
         address: ManagedAddress,
-        token: OptionalValue<TokenIdentifier>,
+        _token: OptionalValue<TokenIdentifier>,
     ) -> BigUint {
-        self.members().get(&address).unwrap_or_default()
+        self.get_member_tier_internal(&address)
+            .map_or_else(BigUint::zero, |tier| tier.voting_power)
     }
 
     #[view(getDaoMembers)]
     fn get_dao_members_view(
         &self,
-        token: OptionalValue<TokenIdentifier>,
+        _token: OptionalValue<TokenIdentifier>,
     ) -> MultiValueEncoded<MultiValue2<ManagedAddress, BigUint>> {
-        let mut members_multi = MultiValueEncoded::new();
-
-        for (address, points) in self.members().iter() {
-            members_multi.push((address, points).into());
-        }
-
-        members_multi.into()
+        self.members()
+            .iter()
+            .map(|(address, points)| (address, points).into())
+            .collect()
     }
 
     #[endpoint(addPoints)]
     fn add_points_endpoint(&self, address: ManagedAddress, amount: BigUint) {
         self.require_caller_is_manager();
-
-        let current_points = self.members().get(&address).unwrap_or_default();
-        let new_points = current_points + amount;
-
+        let new_points = self.members().get(&address).unwrap_or_default() + amount;
         self.members().insert(address, new_points);
     }
 
     #[endpoint(removePoints)]
     fn remove_points_endpoint(&self, address: ManagedAddress, amount: BigUint) {
         self.require_caller_is_manager();
-
         let current_points = self.members().get(&address).unwrap_or_default();
 
         if amount > current_points {
@@ -60,10 +63,71 @@ pub trait ExperienceContract {
         self.members().insert(address, current_points - amount);
     }
 
-    fn require_caller_is_manager(&self) {
-        let caller = self.blockchain().get_caller();
+    #[endpoint(addTier)]
+    fn add_tier_endpoint(
+        &self,
+        name: ManagedBuffer,
+        min_threshold: BigUint,
+        voting_power: BigUint,
+    ) {
+        self.require_caller_is_manager();
+        self.tiers().push(&Tier {
+            name,
+            min_threshold,
+            voting_power,
+        });
+    }
 
-        require!(self.managers().contains(&caller), "caller must be manager");
+    #[endpoint(clearAllTiers)]
+    fn clear_all_tiers_endpoint(&self) {
+        self.require_caller_is_manager();
+        self.tiers().clear();
+    }
+
+    #[view(getMemberTier)]
+    fn get_member_tier_view(
+        &self,
+        address: ManagedAddress,
+    ) -> OptionalValue<MultiValue3<ManagedBuffer, BigUint, BigUint>> {
+        self.get_member_tier_internal(&address)
+            .map(|t| (t.name, t.min_threshold, t.voting_power).into())
+            .into()
+    }
+
+    #[view(getAllTiers)]
+    fn get_all_tiers_view(
+        &self,
+    ) -> MultiValueEncoded<MultiValue3<ManagedBuffer, BigUint, BigUint>> {
+        self.tiers()
+            .iter()
+            .map(|tier| (tier.name, tier.min_threshold, tier.voting_power).into())
+            .collect()
+    }
+
+    fn get_member_tier_internal(&self, address: &ManagedAddress) -> Option<Tier<Self::Api>> {
+        let member_points = self.members().get(address).unwrap_or_default();
+        let mut best_tier = None;
+        let mut best_threshold = BigUint::zero();
+
+        for tier in self.tiers().iter() {
+            if member_points < tier.min_threshold {
+                continue;
+            }
+
+            if tier.min_threshold > best_threshold {
+                best_threshold = tier.min_threshold.clone();
+                best_tier = Some(tier.clone());
+            }
+        }
+
+        best_tier
+    }
+
+    fn require_caller_is_manager(&self) {
+        require!(
+            self.managers().contains(&self.blockchain().get_caller()),
+            "caller must be manager"
+        );
     }
 
     #[view(getManagers)]
@@ -72,4 +136,7 @@ pub trait ExperienceContract {
 
     #[storage_mapper("members")]
     fn members(&self) -> MapMapper<ManagedAddress, BigUint>;
+
+    #[storage_mapper("tiers")]
+    fn tiers(&self) -> VecMapper<Tier<Self::Api>>;
 }
